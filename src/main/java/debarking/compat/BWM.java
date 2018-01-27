93825884ce842a5ce37f904b750b74b7615cb782
BeetoGuy <BeetoGuy@gmail.com>
2018-01-26 23:18:05 -0700

    Update BWM compat to latest build

--- src/main/java/debarking/compat/BWM.java
+++ src/main/java/debarking/compat/BWM.java
@@ -5,6 +5,7 @@ import betterwithmods.common.BWOreDictionary;
 import betterwithmods.common.items.ItemBark;
 import betterwithmods.common.items.ItemMaterial;
 import betterwithmods.common.registry.ChoppingRecipe;
+import betterwithmods.common.registry.Wood;
 import betterwithmods.common.registry.blockmeta.managers.KilnManager;
 import betterwithmods.common.registry.blockmeta.managers.SawManager;
 import betterwithmods.module.ModuleLoader;
@@ -24,15 +25,15 @@ public class BWM {
     }
 
     public static void init() {
-        addChopping(new BWOreDictionary.Wood(new ItemStack(DebarkRegistry.oldDebarked, 1, 0), new ItemStack(Blocks.PLANKS, 1, 0), ItemMaterial.getMaterial(ItemMaterial.EnumMaterial.SAWDUST)), "oak_planks");
-        addChopping(new BWOreDictionary.Wood(new ItemStack(DebarkRegistry.oldDebarked, 1, 1), new ItemStack(Blocks.PLANKS, 1, 1), ItemMaterial.getMaterial(ItemMaterial.EnumMaterial.SAWDUST)), "spruce_planks");
-        addChopping(new BWOreDictionary.Wood(new ItemStack(DebarkRegistry.oldDebarked, 1, 2), new ItemStack(Blocks.PLANKS, 1, 2), ItemMaterial.getMaterial(ItemMaterial.EnumMaterial.SAWDUST)), "birch_planks");
-        addChopping(new BWOreDictionary.Wood(new ItemStack(DebarkRegistry.oldDebarked, 1, 3), new ItemStack(Blocks.PLANKS, 1, 3), ItemMaterial.getMaterial(ItemMaterial.EnumMaterial.SAWDUST)), "jungle_planks");
-        addChopping(new BWOreDictionary.Wood(new ItemStack(DebarkRegistry.newDebarked, 1, 0), new ItemStack(Blocks.PLANKS, 1, 4), ItemMaterial.getMaterial(ItemMaterial.EnumMaterial.SAWDUST)), "acacia_planks");
-        addChopping(new BWOreDictionary.Wood(new ItemStack(DebarkRegistry.newDebarked, 1, 1), new ItemStack(Blocks.PLANKS, 1, 5), ItemMaterial.getMaterial(ItemMaterial.EnumMaterial.SAWDUST)), "dark_oak_planks");
+        addChopping(new Wood(new ItemStack(DebarkRegistry.oldDebarked, 1, 0), new ItemStack(Blocks.PLANKS, 1, 0), ItemMaterial.getMaterial(ItemMaterial.EnumMaterial.SAWDUST)), "oak_planks");
+        addChopping(new Wood(new ItemStack(DebarkRegistry.oldDebarked, 1, 1), new ItemStack(Blocks.PLANKS, 1, 1), ItemMaterial.getMaterial(ItemMaterial.EnumMaterial.SAWDUST)), "spruce_planks");
+        addChopping(new Wood(new ItemStack(DebarkRegistry.oldDebarked, 1, 2), new ItemStack(Blocks.PLANKS, 1, 2), ItemMaterial.getMaterial(ItemMaterial.EnumMaterial.SAWDUST)), "birch_planks");
+        addChopping(new Wood(new ItemStack(DebarkRegistry.oldDebarked, 1, 3), new ItemStack(Blocks.PLANKS, 1, 3), ItemMaterial.getMaterial(ItemMaterial.EnumMaterial.SAWDUST)), "jungle_planks");
+        addChopping(new Wood(new ItemStack(DebarkRegistry.newDebarked, 1, 0), new ItemStack(Blocks.PLANKS, 1, 4), ItemMaterial.getMaterial(ItemMaterial.EnumMaterial.SAWDUST)), "acacia_planks");
+        addChopping(new Wood(new ItemStack(DebarkRegistry.newDebarked, 1, 1), new ItemStack(Blocks.PLANKS, 1, 5), ItemMaterial.getMaterial(ItemMaterial.EnumMaterial.SAWDUST)), "dark_oak_planks");
     }
 
-    private static void addChopping(BWOreDictionary.Wood wood, String woodType) {
+    private static void addChopping(Wood wood, String woodType) {
         System.out.println("Adding BWM recipes for plank type: " + woodType);
         BWOreDictionary.woods.add(wood);
         SawManager.WOOD_SAW.addRecipe(wood.getLog(1), wood.getPlank(isHCLumber ? 4 : 5), wood.getSawdust(3));
